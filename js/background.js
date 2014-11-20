782f0bf1f780a879e584bc64d40a11c079c7246d
simpleapples <zangzhiya@gmail.com>
2014-11-20 17:03:05 +0800

    update

--- js/background.js
+++ js/background.js
@@ -1,9 +1,5 @@
 function getShortUrl(url, callback) {
-	$.ajax({
-		url: 'http://dwz.cn/create.php',
-		data: {url: url},
-		success: function(result) {
-			callback(result);
-		}
+	$.post('http://dwz.cn/create.php', {url: encodeURIComponent(url)}, function(result) {
+		callback(result);
 	});
 }
