f333abfd8f22623f75f5766c81dbe5dd1c821354
simpleapples <zangzhiya@gmail.com>
2014-11-21 17:53:16 +0800

    update

--- js/popup.js
+++ js/popup.js
@@ -8,10 +8,16 @@ chrome.tabs.getSelected(null, function(tab) {
 	backgroundPage.getShortUrl(tab.url, function(result) {
 		var jsonObject = JSON.parse(result);
 		if (jsonObject.status == 0 && jsonObject.tinyurl) {
-			$('#short-url-link').attr('href', jsonObject.tinyurl);
-			$('#short-url-link').text(jsonObject.tinyurl);
+			$('#short-url-input').val(jsonObject.tinyurl);
+			$('#click-copy').show();
 		} else {
-			$('#short-url-link').text('该页没有短网址');
+			$('#short-url-input').val('该页没有短网址').attr('readonly', true);
 		}
 	});
+});
+
+$('#click-copy').click(function() {
+	$('#short-url-input').select();
+	document.execCommand('Copy');
+	$('#click-copy').text('完成');
 });
