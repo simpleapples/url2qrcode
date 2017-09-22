73f8f7d38ceceed574f16460848711d83202389a
simpleapples <zangzhiya@gmail.com>
2017-09-22 17:07:13 +0800

    feat: 增加微博短网址作为备用

--- js/popup.js
+++ js/popup.js
@@ -1,20 +1,70 @@
-chrome.tabs.getSelected(null, function(tab) {
-	$('#qrcode-box').qrcode({
-		width: 200,
-		height: 200,
-		text: tab.url
-	});
-	$.post('http://dwz.cn/create.php', {'url': tab.url, 'alias': '', 'access_type': 'web'}, function(result) {
-		var jsonObject = JSON.parse(result);
-		if (jsonObject.status == 0 && jsonObject.tinyurl) {
-			var tinyUrl = jsonObject.tinyurl;
-			$('#short-url-input').val(tinyUrl);
-			$('#copy-text-input').val(tab.title + ' ' + tinyUrl);
-			$('#click-copy').show();
-		} else {
-			$('#short-url-input').val('该页没有短网址').attr('readonly', true);
+function display(title, shortURL) {
+	$('#short-url-input').val(shortURL);
+	$('#copy-text-input').val(title + ' ' + shortURL);
+	$('#click-copy').show();
+}
+
+function displayError(error) {
+	$('#short-url-input').val(error).attr('readonly', true);
+}
+
+function shortWithBaidu(url, successHandler, errorHandler) {
+	$.ajax({
+		type: 'POST',
+		url: 'http://dwz.cn/create.php',
+		data: {'url': url, 'alias': '', 'access_type': 'web'},
+		success: function(result) {
+			var jsonObject = JSON.parse(result);
+			if (jsonObject['status'] == 0 && jsonObject['tinyurl']) {
+				successHandler(jsonObject['tinyurl'])
+			} else {
+				errorHandler('该页没有短网址');
+			}
+		},
+		error: function(err) {
+			errorHandler('生成失败请重试');
 		}
 	});
+}
+
+function shortWithWeibo(url, successHandler, errorHandler) {
+	$.ajax({
+		type: 'GET',
+		url: 'https://api.weibo.com/2/short_url/shorten.json',
+		data: {'access_token': '2.00cE3vmB0xEHgja95e16ac111N57UC', 'url_long': url},
+		success: function(result) {
+			var jsonObject = result;
+			if (jsonObject.urls && jsonObject.urls[0]) {
+				successHandler(jsonObject.urls[0].url_short);
+			} else {
+				errorHandler('该页没有短网址');
+			}
+		},
+		error: function(err) {
+			errorHandler('生成失败请重试');
+		}
+	});
+}
+
+chrome.tabs.getSelected(null, function(tab) {
+	if (!tab.url.startsWith('http')) {
+		displayError('请打开一个网页');
+	} else {
+		$('#qrcode-box').qrcode({
+			width: 200,
+			height: 200,
+			text: tab.url
+		});
+		shortWithBaidu(tab.url, function(shortURL) {
+			display(tab.title, shortURL);
+		}, function(err) {
+			shortWithWeibo(tab.url, function(shortURL) {
+				display(tab.title, shortURL);
+			}, function(err) {
+				displayError(err);
+			});
+		});
+	}
 });
 
 $('#click-copy').click(function() {
