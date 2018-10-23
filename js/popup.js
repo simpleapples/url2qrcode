d70995cecb33818ea3a642b209459ac362842254
simpleapples <zangzhiya@gmail.com>
2018-10-23 16:08:48 +0800

    feat: update api and permission

--- js/popup.js
+++ js/popup.js
@@ -11,31 +11,13 @@ function displayError(error) {
 function shortWithBaidu(url, successHandler, errorHandler) {
 	$.ajax({
 		type: 'POST',
-		url: 'http://dwz.cn/create.php',
-		data: {'url': url, 'alias': '', 'access_type': 'web'},
+		url: 'https://dwz.cn/admin/create',
+		data: JSON.stringify({url: url}),
+		contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
 		success: function(result) {
-			var jsonObject = JSON.parse(result);
-			if (jsonObject['status'] == 0 && jsonObject['tinyurl']) {
-				successHandler(jsonObject['tinyurl'])
-			} else {
-				errorHandler('该页没有短网址');
-			}
-		},
-		error: function(err) {
-			errorHandler('生成失败请重试');
-		}
-	});
-}
-
-function shortWithWeibo(url, successHandler, errorHandler) {
-	$.ajax({
-		type: 'GET',
-		url: 'https://api.weibo.com/2/short_url/shorten.json',
-		data: {'access_token': '2.00cE3vmB0xEHgja95e16ac111N57UC', 'url_long': url},
-		success: function(result) {
-			var jsonObject = result;
-			if (jsonObject.urls && jsonObject.urls[0]) {
-				successHandler(jsonObject.urls[0].url_short);
+			if (result['Code'] == 0 && result['ShortUrl']) {
+				successHandler(result['ShortUrl'])
 			} else {
 				errorHandler('该页没有短网址');
 			}
@@ -58,17 +40,16 @@ chrome.tabs.getSelected(null, function(tab) {
 		shortWithBaidu(tab.url, function(shortURL) {
 			display(tab.title, shortURL);
 		}, function(err) {
-			shortWithWeibo(tab.url, function(shortURL) {
-				display(tab.title, shortURL);
-			}, function(err) {
-				displayError(err);
-			});
+			displayError(err);
 		});
 	}
 });
 
 $('#click-copy').click(function() {
-	$('#copy-text-input').select();
-	document.execCommand('Copy');
-	$('#click-copy').text('完成');
+	var urlText = $('#copy-text-input').val();
+	navigator.clipboard.writeText(urlText).then(() => {
+		$('#click-copy').text('完成');
+	}).catch((err) => {
+		$('#click-copy').text('失败');
+	});
 });
